f47c3fadef6fbedd87d3e6f5b1965167389c3188
paul didier <paul--didier@live.fr>
2020-05-27 12:32:25 +0200

    hvuy

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+
+  CATEGORY = ["chinese","italian","japanese","french","belgian"]
+  validates :category, inclusion: { in: CATEGORY }
+end
